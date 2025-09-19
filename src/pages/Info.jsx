bed7cae4fcd4cc62d65a3a5d190f879b63149626
juju760 <juju@nhis.or.kr>
2025-09-19 14:11:55 +0900

    맛있는 쉐프 초안

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,24 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PrevButton from "../components/PrevButton";
 import InfoInput from "../components/InfoInput";
 import AddButton from "../components/AddButton";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 
-const Info = () => {
+const Info = ({sendIngredientList}) => {
   // logic
   const history = useNavigate();
   // TODO: set함수 추가하기
-  const [ingredientList] = useState([]); // 사용자가 입력할 재료 목록
+  const [ingredientList, setIngredientList] = useState([]); // 사용자가 입력할 재료 목록
 
   const addIngredient = () => {
-    console.log("재료 추가하기");
-    
+    //input 추가
+    const id = Date.now();
+    const newItem = {
+      id,
+      label: `ingredient${id}`,
+      text: "재료명",
+      value: "" //사용자가 입력할 재료 입력값 
+    };
+
+    setIngredientList((prev)=>[...prev,newItem]);
   };
 
   const handleNext = () => {
+    sendIngredientList(ingredientList);
     history("/chat");
   };
+
+  const handleRemove =(selectedId) => {
+    const filterIngredientList = ingredientList.filter((item)=> item.id !== selectedId);
+    setIngredientList(filterIngredientList);
+  };
+
+  const handleInputChange = (updateItem) => {
+    //console.log("🚀 ~ handelInputChange ~ handelInputChange:", handleInputChange);
+    setIngredientList((prev)=>prev.map((item)=> item.id === updateItem.id ? updateItem : item))
+  }
+
+  //useEffect 용법 3가지 
+  //첫번째: 컴포넌트에 존재하는 모든 state의 값이 변경될때 실행
+  //useEffect(()=>{})
+  //두번째: 컴포넌트가 생성되는 딱 한번 실행
+  //useEffect(()=>{},[])
+  //세번재: 특정state가 변경될때 실행 
+  //useEffect(()=>{},[ingredientList])
+  useEffect(()=>{
+    //console.log("ingredientList",ingredientList)
+  },[ingredientList])
 
   // view
   return (
@@ -42,7 +72,7 @@ const Info = () => {
             {/* START:input 영역 */}
             <div>
               {ingredientList.map((item) => (
-                <InfoInput key={item.id} content={item} />
+                <InfoInput key={item.id} content={item} onRemove={handleRemove} onChange={handleInputChange}/>
               ))}
             </div>
             {/* END:input 영역 */}
